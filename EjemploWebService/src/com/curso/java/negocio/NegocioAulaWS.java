4cd79db00e3eec8bbedd08e1f0356cdfdcabc1ca
lutarik <luist.lorca@gmail.com>
2015-05-04 12:27:07 +0200

    WebServices2

---
+++ EjemploWebService/src/com/curso/java/negocio/NegocioAulaWS.java
@@ -0,0 +1,27 @@
+package com.curso.java.negocio;
+
+import com.curso.java.model.Aula;
+
+
+public class NegocioAulaWS {
+
+	void createAula(Aula aula){
+		
+	}
+	
+	Aula readAula(String id){
+		return null;
+	}
+	
+	Aula[] readAulas(){
+		return null;
+	}
+	
+	void updateAula(Aula aula){
+		
+	}
+	
+	void deleteAula(String id){
+		
+	}
+}
